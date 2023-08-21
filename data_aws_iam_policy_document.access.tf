cc13eefa80fb58f7babf5e2b916d422c736bdf57
Dmitriy Novash <novashdima@gmail.com>
2023-08-21 21:05:40 +0300

    fix policy

--- data_aws_iam_policy_document.access.tf
+++ data_aws_iam_policy_document.access.tf
@@ -19,8 +19,23 @@ data "aws_iam_policy_document" "access" {
     }
 
     principals {
-      identifiers = ["acm-pca.amazonaws.com"]
       type        = "Service"
+      identifiers = ["acm-pca.amazonaws.com"]
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.crl.arn}/*"
+    ]
+
+    principals {
+      type = "*"
+      identifiers = ["*"]
     }
   }
 }
