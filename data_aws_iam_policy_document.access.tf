8198f524f7a6731a6fbd5118270401ded8641548
Dmitriy Novash <novashdima@gmail.com>
2023-08-23 15:34:14 +0300

    more secure policy

--- data_aws_iam_policy_document.access.tf
+++ data_aws_iam_policy_document.access.tf
@@ -9,13 +9,13 @@ data "aws_iam_policy_document" "access" {
 
     resources = [
       aws_s3_bucket.crl.arn,
-      "${aws_s3_bucket.crl.arn}/*",
+      "${aws_s3_bucket.crl.arn}/*"
     ]
 
     condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [data.aws_caller_identity.current.account_id]
+      test     = "StringLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:acm-pca:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:certificate-authority*"]
     }
 
     principals {
@@ -30,12 +30,29 @@ data "aws_iam_policy_document" "access" {
     ]
 
     resources = [
+      aws_s3_bucket.crl.arn,
       "${aws_s3_bucket.crl.arn}/*"
     ]
 
     principals {
       type = "*"
       identifiers = ["*"]
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      aws_s3_bucket.crl.arn,
+      "${aws_s3_bucket.crl.arn}/*"
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = ["${data.aws_caller_identity.current.arn}"]
     }
   }
 }
