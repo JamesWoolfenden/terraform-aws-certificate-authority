77ab5a0ffa37cdcdf549d4ef4270c573cf1d7913
Dmitriy Novash <novashdima@gmail.com>
2023-08-24 14:31:01 +0300

    add outputs

--- example/examplea/outputs.tf
+++ example/examplea/outputs.tf
@@ -8,3 +8,13 @@ output "certificate_authority_arn" {
   description = "Certificate authority arn"
   value       = module.ca.certificate_authority_arn
 }
+
+output "certificate" {
+  description = "Root certificate"
+  value       = module.ca.certificate
+}
+
+output "certificate_chain" {
+  description = "Certificate chain"
+  value       = module.ca.certificate_chain
+}
--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,13 @@ output "certificate_authority_arn" {
   description = "Certificate authority arn"
   value       = aws_acmpca_certificate_authority.certificate_authority.arn
 }
+
+output "certificate" {
+  description = "Root certificate"
+  value       = aws_acmpca_certificate.certificate.certificate
+}
+
+output "certificate_chain" {
+  description = "Certificate chain up to root CA"
+  value       = aws_acmpca_certificate.certificate.certificate_chain
+}
