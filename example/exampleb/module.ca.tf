5372279ad3af7c73915672b74aa7575a8433d899
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 08:52:02 +0100

    chore: pin dependencies to immutable SHAs via ghat

--- example/exampleb/module.ca.tf
+++ example/exampleb/module.ca.tf
@@ -1,5 +1,5 @@
 module "ca" {
-  source    = "git::https://github.com/JamesWoolfenden/terraform-aws-certificate-authority.git?ref=ac7e4b8f846704fa5655bcc3d4bdb6640f070f42"
+  source = "git::https://github.com/JamesWoolfenden/terraform-aws-certificate-authority.git?ref=ac7e4b8f846704fa5655bcc3d4bdb6640f070f42" #ac7e4b8f846704fa5655bcc3d4bdb6640f070f42
   algorithm = var.algorithm
   subject   = var.subject
 }
