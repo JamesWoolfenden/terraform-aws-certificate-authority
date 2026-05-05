f4e0dfc38b03c69ca8a09cb24e7c9b5bf4b1ab3e
James Woolfenden <james.woolfenden@gmail.com>
2026-05-05 10:42:39 +0100

    chore: add ghat+actionlint hooks, suppress CRL bucket checkov findings

--- aws_s3_bucket_acl.crl.tf
+++ aws_s3_bucket_acl.crl.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket_acl" "crl" {
   # checkov:skip=CKV_AWS_20: Required
+  # checkov:skip=CKV_AWS_379: CRL must be publicly readable
   bucket = aws_s3_bucket.crl.id
   acl    = "public-read"
 }
--- aws_s3_bucket_ownership_controls.crl.tf
+++ aws_s3_bucket_ownership_controls.crl.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket_ownership_controls" "crl" {
+  # checkov:skip=CKV2_AWS_65: ACLs required for ACM PCA CRL writes
   bucket = aws_s3_bucket.crl.id
 
   rule {
